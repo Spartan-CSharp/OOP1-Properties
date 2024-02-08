0071ca9674a52a1981cbe456e0ac196850f16b9a
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 10:38:47 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			AddressModel address = new AddressModel("123 Some Street", "Scranton", "PA", "12345-6789");
 
